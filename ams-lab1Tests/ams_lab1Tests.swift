ccba2c7221c0b6ca6d2069d8b1c8c87af94279c2
Tomasz Frankiewicz <tomaszfrankiewicz@Tomaszs-Mac.local>
2017-12-08 02:46:28 -0800

    first commit

---
+++ ams-lab1Tests/ams_lab1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ams_lab1Tests.swift
+//  ams-lab1Tests
+//
+//  Created by Tomasz Frankiewicz on 08/12/2017.
+//  Copyright © 2017 Tomasz Frankiewicz. All rights reserved.
+//
+
+import XCTest
+@testable import ams_lab1
+
+class ams_lab1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
